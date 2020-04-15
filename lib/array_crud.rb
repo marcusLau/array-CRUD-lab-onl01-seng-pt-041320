9ac024809066f830cadb983552a33f75e7226803
Marcus Lau <lau.marcus76@gmail.com>
2020-04-15 17:49:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,28 +7,24 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1]
-  array >> 2
-  # array.push(2)
+  # array >> element
+  array.push(2) 
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [1]
-  array.unshift(2) # adds element to the front of the array
+  array.unshift(element) # adds element to the front of the array
 end
 
 def remove_element_from_end_of_array(array)
-  arr = [1]
   array.pop # removes an element from the END of an array
 end
 
 def remove_element_from_start_of_array(array)
-  arr = [1]
   array.shift # removes an element from the FRONT of an array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
